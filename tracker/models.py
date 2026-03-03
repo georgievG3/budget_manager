49b661e17896a6f1e916a25fb2def48bfdd3918c
Georgi Georgiev <156510257+georgievG3@users.noreply.github.com>
2026-03-03 16:33:43 +0200

    Initial commit

---
+++ tracker/models.py
@@ -0,0 +1,52 @@
+from django.db import models
+from django.conf import settings
+
+# Create your models here.
+class Wallet(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="wallet")
+    current_balance = models.DecimalField(max_digits=12, decimal_places=2, default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.email}'s wallet"
+    
+class Categorgy(models.Model):
+    INCOME = "INCOME"
+    EXPENSE = "EXPENSE"
+
+    CATEGORY_TYPES = [
+        (INCOME, "Income"),
+        (EXPENSE, "Expense"),
+    ]
+
+    name = models.CharField(max_length=100)
+    type = models.CharField(max_length=10, choices=CATEGORY_TYPES)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="categories"
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Transaction(models.Model):
+    INCOME = "INCOME"
+    EXPENSE = "EXPENSE"
+
+    TRANSACTION_TYPES = [
+        (INCOME, "Income"),
+        (EXPENSE, "Expense"),
+    ]
+
+    wallet = models.ForeignKey(Wallet, on_delete=models.CASCADE, related_name="transactions")
+    type = models.CharField(max_length=10, choices=TRANSACTION_TYPES)
+    amount = models.DecimalField(max_digits=12, decimal_places=2)
+    category = models.ForeignKey(Categorgy, on_delete=models.SET_NULL, null=True, blank=True)
+
+    description = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.type} - {self.amount}"
